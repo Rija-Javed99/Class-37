5e8138cacaf1f464213fc0b6b58201e60278c384
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-22 04:20:51 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,14 +1,14 @@
 class Form {
 
   constructor() {
-    input = createInput("Name");
-    button = createButton('Play');
-   greeting = createElement('h2');
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+   this.greeting = createElement('h2');
   }
   hide(){
-    greeting.hide();
-   button.hide();
-    input.hide();
+    this.greeting.hide();
+   this.button.hide();
+    this.input.hide();
   }
 
   display(){
@@ -16,19 +16,19 @@ class Form {
     title.html("Car Racing Game");
     title.position(130, 0);
 
-    input.position(130, 160);
-    button.position(250, 200);
+    this.input.position(130, 160);
+    this.button.position(250, 200);
 
-    button.mousePressed(()=>{
-      input.hide();
-      button.hide();
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
       player.name = this.input.value();
       playerCount+=1;
       player.index = playerCount;
       player.update();
       player.updateCount(playerCount);
-      greeting.html("Hello " + player.name)
-      greeting.position(130, 100);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(130, 100);
     });
 
   }
